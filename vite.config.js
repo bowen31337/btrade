0f54ead1685311af2eb06a12ca1519ab04423048
Bowen Li <superdev@outlook.com.au>
2025-08-24 21:43:48 +1000

    Add GitHub Actions workflow for GitHub Pages deployment

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,9 @@
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-  // no config yet
+  base: process.env.NODE_ENV === 'production' ? '/btrade/' : '/',
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets'
+  }
 });
